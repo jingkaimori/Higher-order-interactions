c86f919ece750d8654a32bd69a73b22b08d035d4
jingkaimori <jingkaimori@gmail.com>
2025-05-01 16:32:53 +0800

    add generator to generate most code of hypergraph characters

--- src/Higher_order_interactions.jl
+++ src/Higher_order_interactions.jl
@@ -1,8 +1,13 @@
 module Higher_order_interactions
-export payoff_of_abstract_symmetric_games, payoff_of_public_good_games_with_convex_benefit, multi_linear_polynomial
 
+export payoff_of_abstract_symmetric_games, payoff_of_public_good_games_with_convex_benefit, multi_linear_polynomial
 include("payoffs.jl")
+
+export Hypergraph,Hypergraph_from_legacy_scalars
 include("hypergraph.jl")
+
+export generate_matrix_related_from_L
+include("hypergraph-param.jl")
 
 
 end
--- src/hypergraph.jl
+++ src/hypergraph.jl
@@ -5,15 +5,20 @@ using Finch
 using Combinatorics
 
 struct Hypergraph
-    edges::Tuple{Vararg{Tensor}}
+    edges::Array{Finch.AbstractTensor}
 end
 
 function Hypergraph_from_legacy_scalars(madj2,madj3)
     local N = size(madj3,1)
     local indices = collect(combinations(1:N, 2))
     local index_dict::Dict = Dict((val,idx) for (idx,val) in enumerate(indices))
-    function get_madj3_column_index(j,k, index_dict)
-        return get(index_dict, [j,k], nothing)
+    function get_madj3_column_index(j,k, index_dict)::Union{Int,Nothing}
+        local res = get(index_dict, [j,k], nothing)
+        if isnothing(res)
+            return get(index_dict, [k,j], nothing)
+        else
+            return res
+        end
     end
 
     local omega_2d::Tensor = Tensor(Dense(SparseList(Element(UInt8(0)))), N, N)
@@ -21,16 +26,14 @@ function Hypergraph_from_legacy_scalars(madj2,madj3)
     @finch begin
         omega_2d .= 0
         for j in _, i in _
-            if (i < j)
-                if madj2[i, j] != 0
-                    omega_2d[i, j] = madj2[i, j]
-                end
+            if madj2[i, j] != 0
+                omega_2d[i, j] = madj2[i, j]
             end
         end
 
         omega_3d .= 0
-        for k in 1:N, j in 1:N, i in 1:N
-            if (i < j < k)
+        for k in 1:N, j in 1:N, i in _
+            if j != k
                 let elem = madj3[i, ~get_madj3_column_index(j,k,index_dict)]
                     if elem != 0
                         omega_3d[i, j, k] = elem
@@ -41,7 +44,7 @@ function Hypergraph_from_legacy_scalars(madj2,madj3)
     end
 
     return Hypergraph(
-        (omega_2d, omega_3d)
+        [Scalar(0), omega_2d, omega_3d]
     )
 end
 
