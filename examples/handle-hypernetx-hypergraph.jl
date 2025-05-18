99043a867b7c59a2e58a6adf8a3bd9142d15ccd6
jingkaimori <jingkaimori@gmail.com>
2025-05-18 19:47:43 +0800

    analyze general properties

--- examples/handle-hypernetx-hypergraph.jl
+++ examples/handle-hypernetx-hypergraph.jl
@@ -2,7 +2,7 @@ using Higher_order_interactions
 using FileIO
 using Symbolics
 
-data = load("tests/data/graphs/erdos-renyi-different-size/92487300-3094-11f0-374e-87dd78b2e2aa.hdf5")
+data = load("tests/data/graphs/erdos-renyi-different-size/924a6ed0-3094-11f0-3b1b-096c96f22ff7.hdf5")
 
 prepared_info = prepare_all(data["graph"], generate_PGG_mul, nothing)
 
@@ -16,14 +16,8 @@ println("deltas: $delta_values")
 println("b_c_ratio: $b_c_ratio_num ")
 
 prepared_info = prepare_all(data["graph"], generate_TPGG_mul, prepared_info)
-(b_c_ratio_expr,) = calculate_b_c_ratio_from_graph_matrix(prepared_info)
+@time (b_c_ratio_expr,) = calculate_b_c_ratio_from_graph_matrix(prepared_info)
 println("TPGG:")
 b_c_ratio_num = substitute((b_c_ratio_expr), Dict())
 println("threshods: all 1")
-println("b_c_ratio: $b_c_ratio_num ")
-
-prepared_info = prepare_all(data["graph"], generate_MSG_mul, prepared_info)
-(b_c_ratio_expr,) = calculate_b_c_ratio_from_graph_matrix(prepared_info)
-println("MSG")
-b_c_ratio_num = substitute((b_c_ratio_expr), Dict())
 println("b_c_ratio: $b_c_ratio_num ")
--- examples/system-analysis-visualization.jl
+++ examples/system-analysis-visualization.jl
@@ -5,6 +5,7 @@ data = load("tests/data/statstics.hdf5")
 
 uuids = data["uuids"]
 gametype = data["gametype"]
+graphtype = data["graphtype"]
 node_deg_avg = data["node_deg_avg"]
 edge_deg_avg = data["edge_deg_avg"]
 clustering_coefficent = data["clustering_coefficent"]
@@ -12,3 +13,41 @@ node_deg_val = data["node_deg_val"]
 edge_deg_val = data["edge_deg_val"]
 clustering_coefficent_val = data["clustering_coefficent_val"]
 b_c_ratios = data["b_c_ratios"]
+
+gametype_mask = Dict{String, BitVector}()
+for game_t in ["PGG", "TPGG", "MSG"]
+    gametype_mask[game_t] = (gametype .== game_t)
+end
+
+graphtype_mask = Dict{String, BitVector}()
+for graph_t in ["well-mixed", "anzhisheng-richclub", "anzhisheng-star", "chung-lu", "erdos-renyi"]
+    graphtype_mask[graph_t] = (graphtype .== graph_t)
+end
+
+default(fontfamily = "SimSunB")
+
+function plot_different_game(title::String, xlabel, xvar)
+    p = plot(title = title, xlabel = xlabel, ylabel = "临界收益比 (b/c)*")
+    for gt in ["PGG", "TPGG", "MSG"]
+        filter1 = findall(gametype_mask[gt])
+        scatter!(p, xvar[filter1], b_c_ratios[filter1], label = gt)
+    end
+    savefig(p, "$title.svg")
+end
+
+p1 = plot(title="不同博弈下临界收益比和节点平均度的关系", xlabel = "节点平均度", ylabel = "临界收益比 (b/c)*")
+for gt in ["PGG", "TPGG", "MSG"]
+    filter1 = findall(gametype_mask[gt] .&& (.! graphtype_mask["well-mixed"]))
+    scatter!(p1, node_deg_avg[filter1], b_c_ratios[filter1], label = gt)
+end
+savefig(p1, "不同博弈下临界收益比和节点平均度的关系.svg")
+
+p2 = plot_different_game("不同博弈下临界收益比和节点度方差的关系", "节点度方差", node_deg_val)
+
+p3= plot_different_game("不同博弈下临界收益比和平均组规模的关系", "平均组规模", edge_deg_avg)
+
+p4=plot_different_game("不同博弈下临界收益比和组规模方差的关系", "组规模方差", edge_deg_val)
+
+p5=plot_different_game("不同博弈下临界收益比和平均聚集系数的关系", "平均聚集系数", clustering_coefficent)
+
+p6=plot_different_game("不同博弈下临界收益比和聚集系数方差的关系", "聚集系数方差", clustering_coefficent_val)
